aa362c994ed6d67119751505242fd9fd94065b09
Vatsal Patel <iamvatsallpatell@gmail.com>
2023-08-20 23:00:27 +0530

    Stage-1 Updated

---
+++ input-output.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main() {
+
+// Output Using printf
+int rollno = 16;
+printf("Roll-No is %d", rollno);
+
+//input using scanf
+int a, b;
+printf("enter a \n");
+scanf("%d", &a);
+printf("enter b \n");
+scanf("%d", &b);
+printf("sum of a & b is : %d \n", a+b);
+
+return 0;
+}
---
+++ variable-datatypes.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main() {
+
+ //Assign Variable
+int name;
+int a;
+int IsValue;
+
+//DataTypes
+float pi = 3.14;
+int age = 22;
+char UpperCase = 'A';
+return 0;
+}
