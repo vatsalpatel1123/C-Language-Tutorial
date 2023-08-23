debc50a4a919ba1a911b17a624f1fa879cd9c2be
Vatsal Patel <iamvatsallpatell@gmail.com>
2023-08-23 23:56:13 +0530

    Stage-7 Multi Array

---
+++ multi-array.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+
+    int arr[][2] = {{1,2},{3,4}};
+    printf("%d\n",arr[0][0]); 
+    printf("%d\n",arr[0][1]); 
+    printf("%d\n",arr[1][0]); 
+    printf("%d\n",arr[1][1]); 
+
+    
+}
