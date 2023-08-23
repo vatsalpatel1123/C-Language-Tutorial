bab32c7e3d7f71dc91bf363e1be032e57df2dd66
Vatsal Patel <iamvatsallpatell@gmail.com>
2023-08-24 00:06:07 +0530

    Stage-8 String

---
+++ string.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+
+    char name[] = {'V','A','T','S','A','L','\0'};
+    char string_name[] = "VATSAL PATEL";
+
+    printf("%s\n",name);
+    printf("%c\n",name[1]);
+
+    printf("%s\n",string_name);
+
+    return 0;
+
+}
