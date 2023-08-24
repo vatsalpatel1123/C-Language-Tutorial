f4456198f7e2533c15a38c9cc8762d88f8a0c935
Vatsal Patel <iamvatsallpatell@gmail.com>
2023-08-24 22:43:41 +0530

    Stage-9 Struct Pointer

---
+++ struct-pointer.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<string.h>
+
+struct information
+    {
+        char name[20];
+        int phone;
+        char city[10];
+        float income;
+
+    };
+
+int main(){
+
+    struct information c1;
+
+    strcpy(c1.name,"Vatsal");
+        c1.phone = 98;
+        c1.income = 38.5;
+        strcpy(c1.city,"Ahmedabad");
+
+    struct information *ptr = &c1;
+        printf("%s \n",(*ptr).name);
+
+    return 0;
+}
